ab09d7927f0e670b5cfe0788dd1bf70ff695d488
Erkin kyzy Aidai <a73613821@gmail.com>
2024-06-07 19:16:08 +0600

    first commit

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 
+# from database.models import models_main
+# import asyncio
+
+
+# async def main():
+#     await models_main()
+
+
+
+# if __name__ == "__main__":
+#     try:
+#         asyncio.run(main())
+#     except:
+#         print("Ошибка")
+
+
 from config import TOKEN
 from database.models import models_main
 import asyncio
-
+from aiogram import Bot, Dispatcher
+from handlers.commands import router
+import logging
+import sys
 
 async def main():
-    await models_main()
+
+    bot = Bot(token=TOKEN)# Подключается
+    dp = Dispatcher()
+    dp.include_router(router=router)
+
+    await dp.start_polling(bot)# Держит бота в активном состоянии
+    # await models_main()
 
 
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,stream=sys.stdout)
 
-if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except:
-        print("Ошибка")
-
-
-
+    except Exception as e:
+        print("Ошибка",e)
 
 
 
